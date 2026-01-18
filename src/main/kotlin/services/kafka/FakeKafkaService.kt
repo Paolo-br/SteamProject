190ca152fd347bf048ffed4c9205cc3ba1ec23f9
Paolo-br <paolobracq06@gmail.com>
2026-01-18 17:16:28 +0100

    Ajout commentaire pour faciliter rapport

--- src/main/kotlin/services/kafka/FakeKafkaService.kt
+++
@@ -1,5 +0,0 @@
-// Supprimé dans la version front-only : ce projet ne contient plus de service Kafka.
-// Le package est conservé vide pour éviter toute référence résiduelle.
-
-package org.example.services.kafka
-
